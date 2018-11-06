7f0680e47b2a4638bc778f97e9ddf0b24a7673ed
rainboylvx <rainboylvx@qq.com>
2018-11-06 20:27:55 +0800

    css分离 热加载 html-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const Webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode:'development',
@@ -8,20 +11,35 @@ module.exports = {
         publicPath:'/dist/',    //公用路径,会影响css里的路径,同样production的时候转成cdn很好用
         path:path.join(__dirname,'dist')
     },
+    plugins:[
+        new Webpack.HotModuleReplacementPlugin(),
+        new ExtractTextPlugin('css/app.css'),
+        new HtmlWebpackPlugin({
+            title:'Hello World',
+            template: './src/index.html', //模板地址
+            filename:'../index.html'
+        }),
+    ],
     module:{
         rules:[
-            { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' },
+            { 
+                test: /\.styl$/, 
+                use: ExtractTextPlugin.extract({
+                    use: [ 'css-loader', 'stylus-loader' ],
+                    publicPath:'../' //解决css图片中的路径问题
+                })
+                //loader: 'style-loader!css-loader!stylus-loader' 
+            },
             {
-                test: /\.(png|jpg)$/,
-                //loader: "url-loader?limit=8192&name=images/[hash:8].[name].[ext]",
-                loader: "url-loader?limit=1&name=images/[hash:8].[name].[ext]",
-                options:{
-                    publicPath:'./images'
-                }
-                //limit参数，代表如果小于大约4k则会自动帮你压缩成base64编码的图片,否则拷贝文件到生产目录
-                //name后面是打包后的路径；
-                //loader 后面 limit 字段代表图片打包限制，这个限制并不是说超过了就不能打包，而是指当图片大小小于限制时会自动转成 base64 码引用
-                //上例中大于8192字节的图片正常打包，小于8192字节的图片以 base64 的方式引用。
+                test:/\.(png|jpg|gif)$/,
+                use:[{
+                    loader:'url-loader',
+                    options:{ // 这里的options选项参数可以定义多大的图片转换为base64
+                        limit:16*1024, // 表示小于16kb的图片转为base64,大于50kb的是路径
+                        outputPath:'images', //定义输出的图片文件夹
+                        name:'[name].[ext]'
+                    }
+                }]
             }
 
         ]
@@ -29,12 +47,14 @@ module.exports = {
     devServer:{ //我们在这里对webpack-dev-server进行配置
         contentBase:'./', //在哪个路径里启动 server
         host:'0.0.0.0',
-        //historyApiFallback:{ //这个对单页面的程序的history api 的404 起作用
-            //rewrites:[{
-                //from:/./,
-                //to:'/'
-            //}]
-        //},
+        /*
+         *historyApiFallback:{ //这个对单页面的程序的history api 的404 起作用
+         *    rewrites:[{
+         *        from:/./,
+         *        to:'/'
+         *    }]
+         *},
+         */
         overlay: true, //错误会显示在html页面上
         stats: "errors-only", //编译的输出
     }
